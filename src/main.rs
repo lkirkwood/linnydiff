1e4b0091b23fc4073ec6eb09083fdba836fee9f6
Linus Kirkwood <linuskirkwood@gmail.com>
2024-10-20 13:38:20 +1100

    add basic cli

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,47 @@
+use clap::Parser;
+use std::{collections::HashMap, fs, path::PathBuf};
+
+use diff::{
+    diff,
+    model::{Edit, EditKind, Slice},
+};
+
 mod diff;
 
+fn print_edits(a: Slice<'_>, b: Slice<'_>, edits: &[Edit<'_>]) {
+    for edit in edits {
+        match edit.kind {
+            EditKind::DELETE => println!("{} -- {}", edit.pos, edit.line),
+            EditKind::INSERT => println!("{} ++ {}", edit.pos, edit.line),
+        }
+    }
+
+    // let edit_map = edits
+    //     .into_iter()
+    //     .map(|edit| ((edit.kind.clone(), edit.pos), edit))
+    //     .collect::<HashMap<_, _>>();
+
+    // for index in 0..a.len().max(b.len()) {
+    //     if let Some(edit) = edit_map.get(&(EditKind::DELETE, index)) {
+    //         println!("{index} -- {}", edit.line);
+    //     }
+    // }
+}
+
+#[derive(Parser)]
+struct Cli {
+    first: PathBuf,
+    second: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    let source = fs::read_to_string("test/notes-1.org").unwrap();
+    let source_lines = source.lines().collect::<Vec<_>>();
+    let target = fs::read_to_string("test/notes-2.org").unwrap();
+    let target_lines = target.lines().collect::<Vec<_>>();
+
+    let edits = diff(&source_lines, &target_lines);
+    print_edits(&source_lines, &target_lines, &edits);
 }
