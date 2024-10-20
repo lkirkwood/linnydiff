54ed524f63a07147204a27a2e57a537fdef21b2b
Linus Kirkwood <linuskirkwood@gmail.com>
2024-10-20 13:38:16 +1100

    update some tests

--- src/diff/tests.rs
+++ src/diff/tests.rs
@@ -1,3 +1,5 @@
+use std::fs;
+
 use super::*;
 use paste::paste;
 
@@ -79,16 +81,84 @@ fn test_midsnake_3() {
 
 #[test]
 fn test_diff_0() {
-    slice_from_str!(source, "sheeptractor");
-    slice_from_str!(target, "spaceheater");
+    slice_from_str!(source, "aaaaa");
+    slice_from_str!(target, "aabbaa");
     let edits = diff(&source, &target);
-    dbg!(edits);
+    assert_eq!(
+        vec![
+            Edit::delete("a", 2),
+            Edit::insert("b", 2),
+            Edit::insert("b", 3)
+        ],
+        edits
+    );
 }
 
 #[test]
 fn test_diff_1() {
-    slice_from_str!(source, "aaaaa");
-    slice_from_str!(target, "aabbaa");
+    slice_from_str!(source, "foobarbazbar");
+    slice_from_str!(target, "foobarbar");
+    let edits = diff(&source, &target);
+    assert_eq!(
+        vec![
+            Edit::delete("z", 8),
+            Edit::delete("b", 9),
+            Edit::delete("a", 10)
+        ],
+        edits
+    );
+}
+
+#[test]
+fn test_diff_2() {
+    slice_from_str!(source, "sheeptractor");
+    slice_from_str!(target, "spaceheater");
     let edits = diff(&source, &target);
     dbg!(edits);
+    assert!(false);
+}
+
+#[test]
+fn test_diff_3() {
+    let source = fs::read_to_string("test/notes-1.org").unwrap();
+    let source_lines = source.lines().collect::<Vec<_>>();
+    let target = fs::read_to_string("test/notes-2.org").unwrap();
+    let target_lines = target.lines().collect::<Vec<_>>();
+    let edits = diff(&source_lines, &target_lines);
+    let desired = vec![
+        Edit::delete("def diff(a: str, b: str):", 17),
+        Edit::delete("    N, M = len(a), len(b)", 18),
+        Edit::delete("    for D in range(0, M + N + 1):", 19),
+        Edit::delete("        for k in range(-D, D + 1):", 20),
+        Edit::delete(
+            "            # Find the endpoint of the furthest reaching D-path in diagonal k",
+            21,
+        ),
+        Edit::delete("            Dpath, endpoint = ...", 22),
+        Edit::delete("            if endpoint == (N, M):", 23),
+        Edit::delete("                return Dpath", 24),
+        Edit::delete("#+end_src", 25),
+        Edit::delete("", 26),
+        Edit::delete("#+begin_src python :results silent", 27),
+        Edit::insert("#+end_src", 24),
+        Edit::delete("class Edit:", 36),
+        Edit::delete("    index: int", 37),
+        Edit::delete("    kind: EditKind", 38),
+        Edit::delete("    char: Optional[str]", 39),
+        Edit::delete("#+end_src", 40),
+        Edit::insert("** Adding some text", 26),
+        Edit::insert("    for D in range(0, M + N + 1):", 27),
+        Edit::insert("        for k in range(-D, D + 1, 2):", 28),
+        Edit::insert(
+            "            if k == -D or (k != D and V.get(k - 1, -1) < V.get(k + 1, -1)):",
+            29,
+        ),
+        Edit::insert("                x = V[k + 1]", 30),
+        Edit::insert("            else:", 31),
+        Edit::insert("                x = V[k - 1] + 1", 32),
+        Edit::insert("", 33),
+        Edit::insert("            y = x - k", 34),
+    ];
+
+    assert_eq!(desired, edits);
 }
